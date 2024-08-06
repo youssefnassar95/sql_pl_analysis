7985298b4274cfe84e0d342e99ffa56c9f7a30eb
youssefnassar95 <youssefnassar95@gmail.com>
2024-08-06 13:13:12 +0200

    Best season for every team

--- premier_league.sql
+++ premier_league.sql
@@ -99,3 +99,49 @@ select * from away_wins
 order by AwayWins DESC;
 /* Only one team in Premier League history has achieved 16 out of 19 away wins in a single season. 
 This team is Man City in 2017/2018 season.*/
+
+
+/* Creating detailed overview of team's performance over all seasons from both new created tables
+COALESCE to get first non-null selection (create one column from two columns)*/
+select
+    coalesce(a."Season_End_Year", h."Season_End_Year") as Season_End_Year,
+    coalesce(a."Away", h."Home") as Team,
+    h.HomeWins, a.AwayWins,
+    h.HomeDraws, a.AwayDraws,
+	h.HomeLoses, a.AwayLoses,
+    h.HomeGoalsScored, a.AwayGoalsScored,
+    h.HomeGoalsConceded, a.AwayGoalsConceded
+from away_wins a
+left join home_wins h
+    on a."Season_End_Year" = h."Season_End_Year"
+    and a."Away" = h."Home";
+
+-- Creating short overview of team's performance over all seasons from both new created tables
+create table perf_overview As
+select
+    coalesce(a."Season_End_Year", h."Season_End_Year") as Season_End_Year,
+    coalesce(a."Away", h."Home") as Team,
+    h.HomeWins +  a.AwayWins as Wins,
+    h.HomeDraws +  a.AwayDraws  as Draws,
+	h.HomeLoses +  a.AwayLoses as Loses,
+    h.HomeGoalsScored + a.AwayGoalsScored as GoalsScored,
+    h.HomeGoalsConceded + a.AwayGoalsConceded as GoalsConceded,
+    3*(h.HomeWins +  a.AwayWins) + 1*(h.HomeDraws +  a.AwayDraws) as Points
+from away_wins a
+left join home_wins h
+    on a."Season_End_Year" = h."Season_End_Year"
+    and a."Away" = h."Home";
+
+
+/*Finding the best season for every team in PL history in terms of points gained
+To get the complete overview of the best season for each team I used subquery that retrieves the maximum points for each team and the corresponding season.
+Than I joined that subquery with original table.*/
+select p.*
+from perf_overview as p
+inner join (
+  select Team, max(Points) as MaxPoints
+  from perf_overview
+  group by Team
+) as max_points
+on p.Team = max_points.Team and p.Points = max_points.MaxPoints
+order by Points DESC;
